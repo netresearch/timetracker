b981a8a620f8c51a2a18b2121a98a4f9324acf40
Tony Kreißl <kreissl@mogic.com>
2016-08-12 15:26:12 +0200

    make activities configurable via frontend

--- src/Netresearch/TimeTrackerBundle/Resources/public/js/netresearch/store/Activities.js
+++ src/Netresearch/TimeTrackerBundle/Resources/public/js/netresearch/store/Activities.js
@@ -4,11 +4,12 @@ Ext.define('Netresearch.store.Activities', {
 	requires: [
    	    'Netresearch.model.Activity'
    	],
-	
+
     autoLoad: true,
     model: 'Netresearch.model.Activity',
     proxy: {
-        type: 'memory',
+        type: 'ajax',
+        url: url + 'getActivities',
         reader: {
             type: 'json',
             record: 'activity'
@@ -17,21 +18,5 @@ Ext.define('Netresearch.store.Activities', {
     sorters: [{
             property: 'name',
             direction:'ASC'
-    }],
-
-    /* Read data from json var in html source code */
-    load: function() {
-        var newData = [], record;
-        for (var key in activityData) {
-            record = activityData[key].activity;
-
-            if (!(record instanceof Ext.data.Model)) {
-                record = Ext.ModelManager.create(record, this.model);
-            }
-            newData.push(record);
-        }
-
-        // False means: remove old data from store
-        this.loadRecords(newData, {addRecords: false});
-    }
+    }]
 });
