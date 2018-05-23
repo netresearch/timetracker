834f8beb1ff2488eea10ab815c68fbf6c516ab61
Tobias.Hein <tobias.hein@netresearch.de>
2018-05-23 08:50:49 +0200

    NRTECH-3720: extend the app_dev check for hostname from timetracker.sobol.nr to *timetracker.sobol.nr - make debugging possible for developer timetracker vhosts

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if ($_SERVER['HTTP_HOST'] !== 'timetracker.sobol.nr'
+if (strpos($_SERVER['HTTP_HOST'],'timetracker.sobol.nr') === FALSE
     &&  ( isset($_SERVER['HTTP_CLIENT_IP'])
         || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
         || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
