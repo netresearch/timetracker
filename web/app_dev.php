fb3f931d7a9e69a13ea5b26a5663dc5dfe66ec95
Christian Weiske <cweiske@cweiske.de>
2021-01-19 14:45:58 +0100

    Allow app_dev.php when using "localhost" as host (for development with the docker image)

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (strpos($_SERVER['HTTP_HOST'],'timetracker.sobol.nr') === FALSE
+    && strpos($_SERVER['HTTP_HOST'], 'localhost') === FALSE
     &&  ( isset($_SERVER['HTTP_CLIENT_IP'])
         || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
         || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
