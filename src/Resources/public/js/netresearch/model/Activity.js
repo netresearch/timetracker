4cb87ca5beefa63e2d24f9b007627c998eb845f4
Sebastian Mendel <sebastian.mendel@netresearch.de>
2022-01-04 14:47:11 +0100

    clean up files - already copied to right place

--- src/Resources/public/js/netresearch/model/Activity.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Netresearch.model.Activity', {
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'id', type: 'integer'},
-        {name: 'name', type: 'string'},
-        {name: 'needsTicket', type: 'boolean'},
-        {name: 'factor', type: 'float'}
-    ]
-});
