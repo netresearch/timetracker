ed96bf152bed914d3d35fccfb7fa21ad2be4de6c
Sebastian Mendel <sebastian.mendel@netresearch.de>
2022-01-07 09:13:12 +0100

    current state of rector

--- rector.php
+++ rector.php
@@ -17,7 +17,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
-        __DIR__ . '/templates',
+        //__DIR__ . '/templates',
         //__DIR__ . '/tests',
     ]);
     $parameters->set(
@@ -33,34 +33,35 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // run them, one by one
 
     // PHP Upgrade
-    //$containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
 
     // Symfony Upgrade
-    //$containerConfigurator->import(SymfonySetList::SYMFONY_50_TYPES);
-    //$containerConfigurator->import(SymfonySetList::SYMFONY_52_VALIDATOR_ATTRIBUTES);
-    //$containerConfigurator->import(SymfonySetList::SYMFONY_60);
+    $containerConfigurator->import(SymfonyLevelSetList::UP_TO_SYMFONY_60);
 
-    //$containerConfigurator->import(SymfonySetList::SYMFONY_STRICT);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_50_TYPES);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_52_VALIDATOR_ATTRIBUTES);
 
-    //$containerConfigurator->import(SymfonyLevelSetList::UP_TO_SYMFONY_54);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_STRICT);
 
-    //$containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    // FAIL:   Class App:Project was not found while trying to analyse it - discovering symbols is probably not configured properly.
     //$containerConfigurator->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
-    //$containerConfigurator->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $containerConfigurator->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
 
     // Doctrine
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_25);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_BEHAVIORS_20);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_COMMON_20);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_210);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_211);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_30);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_GEDMO_TO_KNPLABS);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_25);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_BEHAVIORS_20);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_COMMON_20);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_210);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_211);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_30);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_GEDMO_TO_KNPLABS);
+    // FAILS: An entity was not found for "App\Repository\ActivityRepository" repository.
     //$containerConfigurator->import(DoctrineSetList::DOCTRINE_REPOSITORY_AS_SERVICE);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
-    //$containerConfigurator->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
-    //$containerConfigurator->import(DoctrineSetList::DOCTRINE_ODM_23);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
+    $containerConfigurator->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ODM_23);
 
     // Twig
     //$containerConfigurator->import(TwigSetList::TWIG_112);
@@ -71,9 +72,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     //$containerConfigurator->import(TwigSetList::TWIG_240 );
     $containerConfigurator->import(TwigSetList::TWIG_UNDERSCORE_TO_NAMESPACE);
 
-    //$containerConfigurator->import(PHPOfficeSetList::PHPEXCEL_TO_PHPSPREADSHEET);
+    $containerConfigurator->import(PHPOfficeSetList::PHPEXCEL_TO_PHPSPREADSHEET);
 
-    //Rector\Autodiscovery\Rector\Class_\MoveEntitiesToEntityDirectoryRector
-    $containerConfigurator->import(\Rector\Autodiscovery\Rector\Class_\MoveEntitiesToEntityDirectoryRector);
 
+    // register single rule
+    $services->set(\Rector\Autodiscovery\Rector\Class_\MoveEntitiesToEntityDirectoryRector::class);
 };
