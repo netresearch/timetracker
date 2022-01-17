9ef44a767ea1968ef6ca1a17571c6a73c7b39309
Sebastian Mendel <sebastian.mendel@netresearch.de>
2022-01-17 18:02:33 +0100

    add some fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,13 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Activity;
+use App\Entity\Customer;
+use App\Entity\Project;
+use App\Entity\Team;
+use App\Entity\User;
+use App\Entity\User\Types;
+use App\Repository\ActivityRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +16,34 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $manager->persist((new Activity)->setName(Activity::SICK));
+        $manager->persist((new Activity)->setName(Activity::HOLIDAY));
+        $manager->persist((new Activity)->setName('Work'));
+
+        $user = (new User)
+            ->setUsername('admin')
+            ->setAbbr('ADM')
+            ->setType(Types::PL)
+            ->setRoles(['ROLE_PL']);
+        $manager->persist($user);
+
+        $team = (new Team)
+            ->setName('Default')
+            ->setLeadUser($user);
+        $manager->persist($team);
+
+        $customer = (new Customer)
+            ->setName('ACME')
+            ->setActive(true)
+            ->setGlobal(true);
+        $manager->persist($customer);
+
+        $project = (new Project)
+            ->setName('Website')
+            ->setCustomer($customer)
+            ->setGlobal(true)
+            ->setActive(true);
+        $manager->persist($project);
 
         $manager->flush();
     }
