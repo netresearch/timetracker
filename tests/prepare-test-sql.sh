dac1816bdd24d7d01942a48832c5319242f654bd
Timothy Elias <elias@mogic.com>
2024-10-24 13:13:32 +0200

    TIM-111: Fix local build on MacOS
    Previous sed command throws error on a mac:
    > sed: 1: "../sql/unittest/001_tes ...": invalid command code .

--- tests/prepare-test-sql.sh
+++ tests/prepare-test-sql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #   run this script to duplicate the database structure
 #   the duplicate is used to create a test env.
+#   command runs on linux and mac
 
-cp ../sql/full.sql ../sql/unittest/001_testtables.sql
-sed -i '1s/^/USE unittest;\n/' ../sql/unittest/001_testtables.sql
+sed '1s/^/USE unittest;\n/' ../sql/full.sql > ../sql/unittest/001_testtables.sql
