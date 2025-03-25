0dc87def6b4ccd57cc64a477cf0e9915b730414c
Sebastian Mendel <info@sebastianmendel.de>
2025-03-25 19:20:34 +0100

    add rector

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_82,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+    ]);
+
+    $rectorConfig->skip([
+        // Skip certain files or directories if needed
+    ]);
+};
