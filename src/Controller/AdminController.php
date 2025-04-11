2442b5275a288d021f0e3a19bcaac84c5a1dd988
Sebastian Mendel <info@sebastianmednel.de>
2025-04-12 01:31:34 +0200

    refactor: Remove preset and contract methods from AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -26,6 +26,21 @@ use App\Services\SubticketSyncService;
  */
 class AdminController extends BaseController
 {
+    /**
+     * Logger property for the controller
+     */
+    protected $logger;
+
+    /**
+     * Sets the logger for this controller
+     *
+     * @required
+     */
+    public function setLogger(\Psr\Log\LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
+
     public function getCustomersAction(Request $request): \App\Model\Response|\App\Model\JsonResponse
     {
         if (!$this->checkLogin($request)) {
@@ -60,18 +75,6 @@ class AdminController extends BaseController
         $objectRepository = $this->doctrineRegistry->getRepository(Team::class);
 
         return new JsonResponse($objectRepository->findAll());
-    }
-
-    public function getPresetsAction(Request $request): \App\Model\Response|\App\Model\JsonResponse
-    {
-        if (!$this->checkLogin($request)) {
-            return $this->getFailedLoginResponse();
-        }
-
-        /** @var \App\Repository\PresetRepository $objectRepository */
-        $objectRepository = $this->doctrineRegistry->getRepository(Preset::class);
-
-        return new JsonResponse($objectRepository->getAllPresets());
     }
 
     public function saveProjectAction(Request $request): \App\Model\Response|\App\Model\JsonResponse
@@ -489,94 +492,6 @@ class AdminController extends BaseController
         return new JsonResponse(['success' => true]);
     }
 
-    public function deletePresetAction(Request $request): \App\Model\Response|\App\Response\Error|\App\Model\JsonResponse
-    {
-        if (false === $this->isPl($request)) {
-            return $this->getFailedAuthorizationResponse();
-        }
-
-        try {
-            $id = (int) $request->get('id');
-            $doctrine = $this->doctrineRegistry;
-
-            $preset = $doctrine->getRepository(Preset::class)
-                    ->find($id);
-
-            $em = $doctrine->getManager();
-            $em->remove($preset);
-            $em->flush();
-        } catch (\Exception $exception) {
-            $reason = '';
-            if (str_contains($exception->getMessage(), 'Integrity constraint violation')) {
-                $reason = $this->translate('Other datasets refer to this one.');
-            }
-
-            $msg = sprintf($this->translate('Dataset could not be removed. %s'), $reason);
-            return new Error($msg, 422);
-        }
-
-        return new JsonResponse(['success' => true]);
-    }
-
-    /**
-     * @return Response
-     */
-    public function savePresetAction(Request $request): \App\Model\Response|\App\Response\Error|\App\Model\JsonResponse
-    {
-        if (false === $this->isPl($request)) {
-            return $this->getFailedAuthorizationResponse();
-        }
-
-        $id          = (int) $request->get('id');
-        $name        = $request->get('name');
-        $customer    = $this->doctrineRegistry->getRepository(Customer::class)
-            ->find($request->get('customer'));
-        $project     = $this->doctrineRegistry->getRepository(Project::class)
-            ->find($request->get('project'));
-        $activity    = $this->doctrineRegistry->getRepository(Activity::class)
-            ->find($request->get('activity'));
-        $description = $request->get('description');
-
-        if (strlen((string) $name) < 3) {
-            $response = new Response($this->translate('Please provide a valid preset name with at least 3 letters.'));
-            $response->setStatusCode(406);
-            return $response;
-        }
-
-        $objectRepository = $this->doctrineRegistry->getRepository(Preset::class);
-
-        if ($id !== 0) {
-            $preset = $objectRepository->find($id);
-            if (!$preset) {
-                $message = $this->translator->trans('No entry for id.');
-                return new Error($message, 404);
-            }
-        } else {
-            $preset = new Preset();
-        }
-
-        try {
-            $preset->setName($name)
-                ->setCustomer($customer)
-                ->setProject($project)
-                ->setActivity($activity)
-                ->setDescription($description);
-
-            $em = $this->doctrineRegistry->getManager();
-            $em->persist($preset);
-            $em->flush();
-        } catch (\Exception) {
-            $response = new Response($this->translate('Please choose a customer, a project and an activity.'));
-            $response->setStatusCode(403);
-            return $response;
-        }
-
-        return new JsonResponse($preset->toArray());
-    }
-
-    /**
-     * @return Response
-     */
     public function saveTeamAction(Request $request): \App\Model\Response|\App\Response\Error|\App\Model\JsonResponse
     {
         if (false === $this->isPl($request)) {
@@ -673,7 +588,7 @@ class AdminController extends BaseController
         return new JsonResponse(['success' => true]);
     }
 
-    public function jiraSyncEntriesAction(Request $request): \App\Model\Response|\App\Model\JsonResponse
+    public function jiraSyncEntriesAction(Request $request): \App\Model\Response|\App\Model\JsonResponse|\App\Response\Error
     {
         if (false === $this->isPl($request)) {
             return $this->getFailedAuthorizationResponse();
@@ -741,241 +656,5 @@ class AdminController extends BaseController
             'message' => $syncPrepMsg,
         ];
         return new JsonResponse($data);
-    }
-
-    public function getContractsAction(Request $request): \App\Model\Response|\App\Model\JsonResponse
-    {
-        if (!$this->checkLogin($request)) {
-            return $this->getFailedLoginResponse();
-        }
-
-        /** @var \App\Repository\ContractRepository $objectRepository */
-        $objectRepository = $this->doctrineRegistry->getRepository(Contract::class);
-
-        return new JsonResponse($objectRepository->getContracts());
-    }
-
-    /**
-     * @return Response
-     * @throws \Exception
-     */
-    public function saveContractAction(Request $request): \App\Model\Response|\App\Response\Error|\App\Model\JsonResponse
-    {
-        if (false === $this->isPl($request)) {
-            return $this->getFailedAuthorizationResponse();
-        }
-
-        $contractId = (int) $request->get('id');
-        $start      = $request->get('start');
-        $end        = $request->get('end');
-        $hours_0    = str_replace(',', '.', $request->get('hours_0'));
-        $hours_1    = str_replace(',', '.', $request->get('hours_1'));
-        $hours_2    = str_replace(',', '.', $request->get('hours_2'));
-        $hours_3    = str_replace(',', '.', $request->get('hours_3'));
-        $hours_4    = str_replace(',', '.', $request->get('hours_4'));
-        $hours_5    = str_replace(',', '.', $request->get('hours_5'));
-        $hours_6    = str_replace(',', '.', $request->get('hours_6'));
-        /** @var User $user */
-        $user       = $request->get('user_id') ?
-            $this->doctrineRegistry->getRepository(User::class)
-                ->find($request->get('user_id'))
-            : null;
-
-        /** @var \App\Repository\ContractRepository $objectRepository */
-        $objectRepository = $this->doctrineRegistry->getRepository(Contract::class);
-
-        /** @var \App\Entity\Contract $contract */
-        if ($contractId !== 0) {
-            $contract = $objectRepository->find($contractId);
-            if (!$contract) {
-                $message = $this->translator->trans('No entry for id.');
-                return new Error($message, 404);
-            }
-        } else {
-            $contract = new Contract();
-        }
-
-        if (!$user) {
-            $response = new Response($this->translate('Please enter a valid user.'));
-            $response->setStatusCode(406);
-            return $response;
-        }
-
-        $dateStart = \DateTime::createFromFormat('Y-m-d', $start ?: '');
-        if (!$dateStart) {
-            $response = new Response($this->translate('Please enter a valid contract start.'));
-            $response->setStatusCode(406);
-            return $response;
-        }
-
-        $dateStart->setDate($dateStart->format('Y'), $dateStart->format('m'), $dateStart->format('d'));
-        $dateStart->setTime(0, 0, 0);
-
-        $dateEnd = \DateTime::createFromFormat('Y-m-d', $end ?: '');
-        if ($dateEnd) {
-            $dateEnd->setDate($dateEnd->format('Y'), $dateEnd->format('m'), $dateEnd->format('d'));
-            $dateEnd->setTime(23, 59, 59);
-
-            if ($dateEnd < $dateStart) {
-                $response = new Response($this->translate('End date has to be greater than the start date.'));
-                $response->setStatusCode(406);
-                return $response;
-            }
-        } else {
-            $dateEnd = null;
-        }
-
-        $contract->setUser($user)
-            ->setStart($dateStart)
-            ->setEnd($dateEnd)
-            ->setHours0($hours_0)
-            ->setHours1($hours_1)
-            ->setHours2($hours_2)
-            ->setHours3($hours_3)
-            ->setHours4($hours_4)
-            ->setHours5($hours_5)
-            ->setHours6($hours_6);
-
-        $objectManager = $this->doctrineRegistry->getManager();
-        $objectManager->persist($contract);
-
-        // when updating a existing contract don't look for other contracts for the user
-        if ($contractId !== 0) {
-            $objectManager->flush();
-            return new JsonResponse([$contract->getId()]);
-        }
-
-        // update old contracts,
-        $responseMessage = $this->updateOldContract($user, $dateStart, $dateEnd);
-        if ($responseMessage !== '' && $responseMessage !== '0') {
-            $response = new Response($responseMessage);
-            $response->setStatusCode(406);
-            return $response;
-        }
-
-        $objectManager->flush();
-        return new JsonResponse([$contract->getId()]);
-    }
-
-    /**
-     * Look for existing contracts for user and update the latest if open-ended
-     * When updating to PHP8 change return type to string|null
-     */
-    protected function updateOldContract(User $user, DateTime $newStartDate, ?DateTime $newEndDate): string
-    {
-        $objectManager = $this->doctrineRegistry->getManager();
-        $objectRepository = $this->doctrineRegistry->getRepository(Contract::class);
-
-        // get existing contracts for the user
-        $contractsOld = $objectRepository->findBy(['user' => $user]);
-
-        if (!$contractsOld) {
-            return "";
-        }
-
-        if ($this->checkOldContractsStartDateOverlap($contractsOld, $newStartDate, $newEndDate)) {
-            return $this->translate('There is already an ongoing contract with a start date in the future that overlaps with the new contract.');
-        }
-
-        if ($this->checkOldContractsEndDateOverlap($contractsOld, $newStartDate)) {
-            return $this->translate('There is already an ongoing contract with a closed end date in the future.');
-        }
-
-        // filter to get only open-ended contracts
-        $contractsOld = array_filter($contractsOld, fn ($n): bool => ($n->getEnd() == null));
-        if (count($contractsOld) > 1) {
-            return $this->translate('There is more than one open-ended contract for the user.');
-        }
-
-        if ($contractsOld === []) {
-            return "";
-        }
-
-        $contractOld = array_values($contractsOld)[0];
-
-        // alter exisiting contract with open end
-        // |--old--(update)
-        //      |--new----(|)->
-        if ($contractOld->getStart() <= $newStartDate) {
-            $oldContractEndDate = clone $newStartDate;
-            $contractOld->setEnd($oldContractEndDate->sub(new \DateInterval('P1D')));
-            $objectManager->persist($contractOld);
-            $objectManager->flush();
-        }
-
-        //skip old contract edit for
-        // |--new--| |--old--(|)-->
-        // and
-        // |--old--| |--new--(|)-->
-        return "";
-    }
-
-    /**
-     * look for old contracts that start during the duration of the new contract
-     *      |--old----->
-     *  |--new--(|)-->
-     */
-    protected function checkOldContractsStartDateOverlap(array $contracts, DateTime $newStartDate, ?DateTime $newEndDate): bool
-    {
-        $filteredContracts = [];
-        foreach ($contracts as $contract) {
-            $startsAfterOrOnNewStartDate = $contract->getStart() >= $newStartDate;
-            $startsBeforeOrOnNewEndDate = ($newEndDate instanceof \DateTime) ? ($contract->getStart() <= $newEndDate) : true;
-
-            if ($startsAfterOrOnNewStartDate && $startsBeforeOrOnNewEndDate) {
-                $filteredContracts[] = $contract;
-            }
-        }
-
-        return (bool) $filteredContracts;
-    }
-
-    /** look for contract with ongoing end
-     * |--old--|
-     *      |--new----->
-     */
-    protected function checkOldContractsEndDateOverlap(array $contracts, DateTime $newStartDate): bool
-    {
-        $filteredContracts = [];
-        foreach ($contracts as $contract) {
-            $startsBeforeOrOnNewDate = $contract->getStart() <= $newStartDate;
-            $endsAfterOrOnNewDate = $contract->getEnd() >= $newStartDate;
-            $hasEndDate = $contract->getEnd() !== null;
-
-            if ($startsBeforeOrOnNewDate && $endsAfterOrOnNewDate && $hasEndDate) {
-                $filteredContracts[] = $contract;
-            }
-        }
-
-        return (bool) $filteredContracts;
-    }
-
-    public function deleteContractAction(Request $request): \App\Model\Response|\App\Response\Error|\App\Model\JsonResponse
-    {
-        if (false === $this->isPl($request)) {
-            return $this->getFailedAuthorizationResponse();
-        }
-
-        try {
-            $id = (int) $request->get('id');
-            $doctrine = $this->doctrineRegistry;
-
-            $contract = $doctrine->getRepository(Contract::class)
-                ->find($id);
-
-            $em = $doctrine->getManager();
-            $em->remove($contract);
-            $em->flush();
-        } catch (\Exception $exception) {
-            $reason = '';
-            if (str_contains($exception->getMessage(), 'Integrity constraint violation')) {
-                $reason = $this->translate('Other datasets refer to this one.');
-            }
-
-            $msg = sprintf($this->translate('Dataset could not be removed. %s'), $reason);
-            return new Error($msg, 422);
-        }
-
-        return new JsonResponse(['success' => true]);
     }
 }
