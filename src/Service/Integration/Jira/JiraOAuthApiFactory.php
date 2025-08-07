629c944c4a0f88c86f9a818abfbf000317273ecc
Sebastian Mendel <info@sebastianmendel.de>
2025-08-07 23:12:35 +0200

    test(perf): speed up suite (filter coverage, DBAL transactions), unskip external-API routes, implement holiday DB tests; inject SubticketSyncService; add JiraOAuthApiFactory for mocking

---
+++ src/Service/Integration/Jira/JiraOAuthApiFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service\Integration\Jira;
+
+use App\Entity\TicketSystem;
+use App\Entity\User;
+use App\Helper\JiraOAuthApi;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Routing\RouterInterface;
+
+class JiraOAuthApiFactory
+{
+    public function __construct(private readonly ManagerRegistry $managerRegistry, private readonly RouterInterface $router)
+    {
+    }
+
+    public function create(User $user, TicketSystem $ticketSystem): JiraOAuthApi
+    {
+        return new JiraOAuthApi($user, $ticketSystem, $this->managerRegistry, $this->router);
+    }
+}
+
+
