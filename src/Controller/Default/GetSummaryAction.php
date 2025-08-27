f8593d5c29b11db0b7288548489f9ad85ee5e033
Sebastian Mendel <sebastian.mendel@netresearch.de>
2025-08-27 14:42:22 +0200

    Default: add invokable actions for index, getTimeSummary, getSummary, getData; deprecate methods in DefaultController

---
+++ src/Controller/Default/GetSummaryAction.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Default;
+
+use App\Controller\BaseController;
+use App\Entity\Entry;
+use App\Model\JsonResponse;
+use App\Response\Error;
+use Symfony\Component\HttpFoundation\Request;
+
+final class GetSummaryAction extends BaseController
+{
+    #[\Symfony\Component\Routing\Attribute\Route(path: '/getSummary', name: '_getSummary_attr', methods: ['POST'])]
+    public function __invoke(Request $request): \Symfony\Component\HttpFoundation\RedirectResponse|\App\Model\Response|JsonResponse|Error
+    {
+        if (!$this->checkLogin($request)) {
+            return $this->login($request);
+        }
+
+        $userId = $this->getUserId($request);
+
+        $data = [
+            'customer' => ['scope' => 'customer', 'name' => '', 'entries' => 0, 'total' => 0, 'own' => 0, 'estimation' => 0],
+            'project' => ['scope' => 'project', 'name' => '', 'entries' => 0, 'total' => 0, 'own' => 0, 'estimation' => 0],
+            'activity' => ['scope' => 'activity', 'name' => '', 'entries' => 0, 'total' => 0, 'own' => 0, 'estimation' => 0],
+            'ticket' => ['scope' => 'ticket', 'name' => '', 'entries' => 0, 'total' => 0, 'own' => 0, 'estimation' => 0],
+        ];
+
+        $entryId = $request->request->get('id');
+        if (null === $entryId || '' === $entryId || false === $entryId) {
+            return new JsonResponse($data);
+        }
+
+        /** @var \App\Repository\EntryRepository $objectRepository */
+        $objectRepository = $this->managerRegistry->getRepository(Entry::class);
+        if (!$objectRepository->find($entryId)) {
+            $message = $this->translator->trans('No entry for id.');
+            return new Error($message, \Symfony\Component\HttpFoundation\Response::HTTP_NOT_FOUND);
+        }
+
+        $data = $objectRepository->getEntrySummary((int) $entryId, $userId, $data);
+
+        if ($data['project']['estimation']) {
+            $data['project']['quota'] = $this->get('App\\Service\\Util\\TimeCalculationService')->formatQuota(
+                $data['project']['total'],
+                $data['project']['estimation']
+            );
+        }
+
+        return new JsonResponse($data);
+    }
+}
+
+
