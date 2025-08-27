ed5f2b6cced4ebc122d9e6f67f55fd0d5dd58656
Sebastian Mendel <info@sebastianmednel.de>
2025-08-27 22:39:28 +0200

    style(strict_types): add declare(strict_types=1) to entities, repositories, services, models, and related classes

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * Class for ticket entities in the timetracker.
--- src/Extension/NrArrayTranslator.php
+++ src/Extension/NrArrayTranslator.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * Json array translator for twig-templates.
