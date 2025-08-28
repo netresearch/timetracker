a0e842b10299785a9a00b0e21c65214e7738543b
Sebastian Mendel <sebastian.mendel@netresearch.de>
2025-08-28 12:29:16 +0200

    auth: use entrypoint redirect in nullable CurrentUser branches

--- src/Controller/Default/GetCustomersAction.php
+++ src/Controller/Default/GetCustomersAction.php
@@ -14,7 +14,7 @@ final class GetCustomersAction extends BaseController
     public function __invoke(Request $request, #[\Symfony\Component\Security\Http\Attribute\CurrentUser] ?\App\Entity\User $user = null): \Symfony\Component\HttpFoundation\RedirectResponse|\App\Model\Response|JsonResponse
     {
         if (null === $user) {
-            return $this->login($request);
+            return $this->redirectToRoute('_login');
         }
 
         $userId = (int) $user->getId();
