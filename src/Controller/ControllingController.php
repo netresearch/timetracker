904705092ef3433f65b9e900aeecd02f5664d474
Sebastian Mendel <sebastian.mendel@netresearch.de>
2025-08-28 11:21:10 +0200

    cleanup: delete legacy DefaultController and ControllingController (invokables cover routes)

--- src/Controller/ControllingController.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Removed legacy ControllingController. The routes are provided by invokable
-// App\Controller\Controlling\ExportAction with BC support.
--- src/Controller/DefaultController.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Removed legacy DefaultController. All routes have been migrated to invokable actions under
-// App\Controller\Default\*.
