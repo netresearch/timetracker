70e5d4ed6f2120ee17dea881f82fe6a5c4b40dc2
Sebastian Mendel <info@sebastianmendel.de>
2025-08-28 10:25:28 +0200

    chore(phpstan): remove unused injected props in status/sync controllers

--- src/Controller/Status/CheckStatusAction.php
+++ src/Controller/Status/CheckStatusAction.php
@@ -10,21 +10,6 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
 final class CheckStatusAction extends BaseController
 {
-    // kept for attribute injection compatibility in other environments
-    private \Symfony\Bundle\SecurityBundle\Security $security;
-    private RequestStack $requestStack;
-
-    #[\Symfony\Contracts\Service\Attribute\Required]
-    public function setSecurity(\Symfony\Bundle\SecurityBundle\Security $security): void
-    {
-        $this->security = $security;
-    }
-
-    #[\Symfony\Contracts\Service\Attribute\Required]
-    public function setRequestStack(RequestStack $requestStack): void
-    {
-        $this->requestStack = $requestStack;
-    }
 
     #[\Symfony\Component\Routing\Attribute\Route(path: '/status/check', name: 'check_status', methods: ['GET'])]
     public function __invoke(Request $request): JsonResponse
